3d3eb181f8e1314acce7b08c375960bb8be02b7a
Renee Cousins <nonarkitten@gmail.com>
2026-01-28 22:20:05 -0700

    Switch to makefile

--- denise/build.sh
+++
@@ -1,5 +0,0 @@
-rm denise.bin denise.txt denise.json || true
-yosys -q -p 'synth_ice40 -top top -json denise.json' src/*.v
-nextpnr-ice40 --hx8k --json denise.json --freq 57 --pcf constraints/denise.pcf --package cb132 --asc denise.txt
-icepack denise.txt denise.bin
-iceprog -d i:0x0403:0x06011 denise.bin
